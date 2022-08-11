9a4306c9bc28a6f46514697e2743d0da3a0048fa
Ramond Julio <ramonjulio9999@gmail.com>
2022-08-11 23:31:51 +0700

    finish responsive

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="grid">
+      <div className='number1'>1</div>
+      <div className='number2'>2</div>
+      <div className='number3'>3</div>
+      <div className='number4'>4</div>
+      <div className='number5'>5</div>
+      <div className='number6'>6</div>
+      <div className='number7'>7</div>
+      <div className='number8'>8</div>
+      <div className='number9'>9</div>
     </div>
   );
 }
